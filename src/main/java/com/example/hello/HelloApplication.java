d94f179816c0eb658d9ecdc2ec26b19e827396a4
bupt391 <nuaa36103@163.com>
2021-11-13 23:37:17 +0800

    测试springboot中的hello,world!!!

--- src/main/java/com/example/hello/HelloApplication.java
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloApplication {
-
+/*
+* 我想尝试一下
+*
+* */
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
